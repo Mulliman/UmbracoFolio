93f545c4cf64fed7486f5adf6ef06517606be673
Sam Mullins <sammullins.co.uk@gmail.com>
2021-01-13 21:08:59 +0000

    Fancy next and previous nav

---
+++ Src/UmbracoFolio/UmbracoFolio.Web/Models/NextPrevious.cs
@@ -0,0 +1,11 @@
+﻿using Umbraco.Core.Models.PublishedContent;
+
+namespace UmbracoFolio.Web.Models
+{
+    public class NextPrevious<T> where T : IPublishedContent
+    {
+        public T Next { get; set; }
+
+        public T Previous { get; set; }
+    }
+}
